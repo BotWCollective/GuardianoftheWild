d68e33aeb09321af03fbff4c384588dfe9da395d
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-10-16 21:16:22 -0700

    add environment variable based config

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,18 +1,22 @@
-use env_logger::Env;
+use env_logger::{Env, Builder};
 use gotw::{Bot, BotConfig};
-use log::info;
+use log::error;
+use std::process::exit;
+use std::env;
 
 fn main() {
-    let env = Env::default();
-    env_logger::init_from_env(env);
+    let env = Env::default().filter("GOTW_LOG");
+    Builder::from_env(env).format(|buf, record| {
+		writeln!(buf, "[{}  {}] ({}): {}", buf.timestamp(), record.level(), record.module_path().unwrap_or(""), record.args())
+    }).init();
     let cfg = BotConfig {
-        username: "guardianofthewild".into(),
-        password: "oauth:1234abcd".into(),
-        channel: "#botwcollective".into(),
-        prefix: "!".into(),
+        username: env::var("GOTW_NICK").unwrap_or_else(|_| {error!("Environment variable GOTW_NICK not found."); exit(1)}),
+        password: env::var("GOTW_PASS").unwrap_or_else(|_| {error!("Environment variable GOTW_PASS not found."); exit(1)}),
+        channel: env::var("GOTW_CHAN").unwrap_or_else(|_| {error!("Environment variable GOTW_CHAN not found."); exit(1)}),
+        prefix: env::var("GOTW_PREF").unwrap_or_else(|_| {error!("Environment variable GOTW_PREF not found."); exit(1)}),
     };
-    let mut bot = Bot::login(cfg).unwrap();
+    let mut bot = Bot::login(cfg).unwrap_or_else(|e| {error!("{:?}", e); exit(1)});
     loop {
-        info!("{:?}", bot.try_parse_message());
+	    bot.try_parse_message().unwrap_or_else(|e| {error!("{:?}", e); None});
     }
 }
