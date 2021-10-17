bf6d74d4740e5e6abf88eba7dc3753208db4f97c
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-10-16 19:50:20 -0700

    run the bot

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,3 +1,18 @@
+use env_logger::Env;
+use gotw::{Bot, BotConfig};
+use log::info;
+
 fn main() {
-    println!("Hello, world!");
+    let env = Env::default();
+    env_logger::init_from_env(env);
+    let cfg = BotConfig {
+        username: "guardianofthewild".into(),
+        password: "oauth:1234abcd".into(),
+        channel: "#botwcollective".into(),
+        prefix: "!".into(),
+    };
+    let mut bot = Bot::login(cfg).unwrap();
+    loop {
+        info!("{:?}", bot.try_parse_message());
+    }
 }
