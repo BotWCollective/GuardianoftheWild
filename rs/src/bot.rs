e7f7aa8c4fce320fc045e68b5062317a0aca53e7
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-10-17 09:49:51 -0700

    add BotConfig::from_env

--- rs/src/bot.rs
+++ rs/src/bot.rs
@@ -2,6 +2,7 @@ use crate::{BotError, BotResult, TwitchIrcClient};
 use log::{debug, info};
 use std::collections::HashMap;
 use std::time::Instant;
+use std::env;
 
 pub struct Bot {
     client: TwitchIrcClient,
@@ -13,10 +14,21 @@ pub struct Bot {
 }
 
 pub struct BotConfig {
-    pub channel: String,
-    pub username: String,
-    pub password: String,
-    pub prefix: String,
+    channel: String,
+    username: String,
+    password: String,
+    prefix: String,
+}
+
+impl BotConfig {
+	pub fn from_env() -> BotResult<Self> {
+		Ok(Self {
+	        username: env::var("GOTW_NICK")?,
+	        password: env::var("GOTW_PASS")?,
+	        channel: env::var("GOTW_CHAN")?,
+	        prefix: env::var("GOTW_PREF")?,
+		})
+	}
 }
 
 #[derive(Debug)]
@@ -72,13 +84,6 @@ impl Bot {
         if !raw.starts_with(&self.prefix) {
             debug!("Message {:?} is not command", raw);
             println!("{}", self.timers[0].elapsed().as_secs());
-            if self.timers[0].elapsed().as_secs() >= 2400 {
-                self.client.send_message("Big thanks to LeftShark_Vevo for hosting this! Be sure to follow the runners in their own channels! | twitch.tv/leftshark_vevo ^_^ twitch.tv/vladisvrau ^_^ twitch.tv/coensi ^_^ twitch.tv/itntpiston |")?;
-                self.timers[0] = Instant::now();
-            } else if self.timers[1].elapsed().as_secs() >= 3600 {
-                self.client.send_message("Everyone is free to be comms <3 pls join Shark Squad if you want to help out! https://discord.gg/FkZuJaJ9eE")?;
-                self.timers[1] = Instant::now();
-            }
             ret = Ok(Some(Message {
                 command: None,
                 args: None,
@@ -90,13 +95,6 @@ impl Bot {
             let mut split = raw.split(' ');
             let command = split.next().unwrap().split_once(&self.prefix).unwrap().1;
             let command = &command[..command.len() - 2];
-            if command == "runners" {
-                self.client.send_message("Big thanks to LeftShark_Vevo for hosting this! Be sure to follow the runners in their own channels! | twitch.tv/leftshark_vevo ^_^ twitch.tv/vladisvrau ^_^ twitch.tv/coensi ^_^ twitch.tv/itntpiston |")?;
-                self.timers[0] = Instant::now();
-            } else if command == "host" {
-                self.client.send_message("Everyone is free to be comms <3 pls join Shark Squad if you want to help out! https://discord.gg/FkZuJaJ9eE")?;
-                self.timers[1] = Instant::now();
-            }
             ret = Ok(Some(Message {
                 command: Some(command.to_string()),
                 args: None,
