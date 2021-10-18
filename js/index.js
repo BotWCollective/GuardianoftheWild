6281fafa595624be3878616563493080ad2aac30
PrinceKomali <37776664+PrinceKomali@users.noreply.github.com>
2021-10-18 18:15:08 -0500

    Finalize currency, add rupee colors, !gamble

--- js/index.js
+++ js/index.js
@@ -7,8 +7,7 @@ let e = require("events");
 let fs = require("fs"); 
 
 let CurrencySystem = class {
-    constructor(type) {
-        this.type=type;
+    constructor() {
         this.dat = {};
     }
     init() {
@@ -39,6 +38,36 @@ let CurrencySystem = class {
         }
         this._writejson();
         return outObj;
+    }
+    getRupee(n) {
+        let green = "<:rupee:899771488792608789>";
+        let blue = "<:rupee5:899770964529807430>";
+        let red = "<:rupee20:899770964454289468>";
+        let purple = "<:rupee50:899770964475265024>";
+        let silver = "<:rupee100:899770964483637328>";
+        let gold = "<:rupee300:899770964630441995>";
+        let r;
+        switch(true) { // bad syntax is a result of me forgetting how to use switch() :I
+            case n < 5:
+                r = green;
+                break;
+            case n < 20: 
+                r =  blue;
+                break;
+            case n < 50:
+                r =  red;
+                break;
+            case n < 100: 
+                r =  purple;
+                break;
+            case n < 300:
+                r =  silver
+                break;
+            case n >= 300:
+                r = gold;
+                break;
+        }
+        return r;
     }
     change(user, amt, d) {
         if(!this.dat[user]) this.dat[user] = {value:0,sm:d};
@@ -113,7 +142,8 @@ const fetch = require("node-fetch");
 const bot = new e;
 let t = new Timeouts();
 let cst = new Timeouts();
-let cs = new CurrencySystem("<:rupee:899416200663158814>");
+let gst = new Timeouts();
+let cs = new CurrencySystem();
 cs.init();
 const tmi_client = new tmi.client({
     identity: {
@@ -166,6 +196,7 @@ djs_client.on("message", message => {
 
 
 bot.on("message", message => {
+    let args = message.content.trim().split(" ");
     let reply = (str) => {
         if(message.twitch) tmi_client.say(message.channel, str);
         if(message.discord) message.channel.send(str);
@@ -200,18 +231,47 @@ bot.on("message", message => {
             message.user.color = default_colors[n % default_colors.length];
         }
     }
+    if(args[0] == "!gamble") {
+        if(gst.check(message.user.identification, 600000)) {
+            let current = cs.get(message.user.identification);
+            if(current <= 0) return reply("Get some " + cs.getRupee(1) + " first!")
+            if(!args[1]) return reply("A second argument is required!");
+            if(args[1].toLowerCase() == "all") args[1] = current;
+            if(isNaN(args[1])) return reply("Argument 1 is not a number!");
+            
+            args[1] = parseInt(args[1]);
+            if(current < args[1]) return reply("You do not have " + args[1] + cs.getRupee(args[1]))
+            if(Math.random()>0.5) {
+                cst.set(message.user.identification);
+                cs.change(message.user.identification, args[1]*-1, message.discord ? "discord" : "twitch") 
+                reply(`You lost ${args[1]}${cs.getRupee(args[1])}. You now have ${cs.get(message.user.identification)}${cs.getRupee(cs.get(message.user.identification))}`);
+            }
+            else {
+                cst.set(message.user.identification);
+                cs.change(message.user.identification, args[1], message.discord ? "discord" : "twitch") 
+                reply(`You won ${args[1]}${cs.getRupee(args[1])}. You now have ${cs.get(message.user.identification)}${cs.getRupee(cs.get(message.user.identification))}`);
+            }
+            return gst.set(message.user.identification);
+
+        }
+        else {
+            let _t = ((gst.get(message.user.identification)+600000)-Date.now());
+            return reply(`You may not play for ${new Date(_t).toISOString().substr(15, 8)}!`)
+        }
+    }
 
     // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
     if((message.twitch && message.channel == "#botwcollective" && !message.self) || (message.discord && !message.user.bot && message.guild && message.guild.id == "731993922414575737")) 
     {
         if(cst.check(message.user.identification, 60000)) {
             cst.set(message.user.identification);
-            cs.change(message.user.identification, 1, message.discord ? "discord" : "twitch");
+            let num = 1;
+            if(cs.get(message.user.identification) < 0) num = cs.get(message.user.identification) * -1; 
+            cs.change(message.user.identification, num, message.discord ? "discord" : "twitch");
         }
     }
     // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-    let args = message.content.trim().split(" ");
     args[0] = args[0].toLowerCase();
     if (args[0] == "!discord") {
         reply("Join the discord server! discord.gg/hylian");
@@ -226,7 +286,8 @@ bot.on("message", message => {
         reply("Go follow Bingothon! https://twitch.tv/bingothon");
     }
     if (args[0] == "!bal" || args[0] == "!balance") {
-        reply(`You have ${cs.get(message.user.identification)} ${cs.type}`)
+        let v = cs.get(message.user.identification);
+        reply(`You have ${v.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}${cs.getRupee(v)}`)
     }
     if (args[0] == "!hug") {
         if(!args[1]) {
@@ -259,7 +320,7 @@ bot.on("message", message => {
                     for(s of SUBPREFIXES){ 
                      if(x.name.startsWith(s)) return false
                    }
-                   return true
+                   return true;
                  }
                 var emotes = message.twitch ? Object.keys(twitchemotes) : message.guild.emojis.cache.array().filter(efilter).map(x=>x.toString())
                 str = str + emotes[Math.floor(Math.random() * emotes.length)] + " "
@@ -273,10 +334,19 @@ bot.on("message", message => {
     }
     if(args[0] == "!lb" || args[0] == "!leaderboard") {
         if (message.twitch) return;
-        let lbsm = args[1] ? args[1] : "all";
-        if (args[1] && (args[1] == "discord" || args[1] != "twitch")) {
-            return reply("Argument 2 must be \`discord\`, \`twitch\` or undefined");
+        let lbsm = "all";
+        let pgct = 1;
+        if(args.length >= 1) {
+            for(i in args){
+                if(i==0) continue;
+                if(!isNaN(args[i])) pgct = parseInt(args[i]);
+                else if(args[i] == "discord") lbsm = "discord";
+                else if(args[i] == "twitch") lbsm = "twitch";
+                else if(args[i] != "all") return reply(`Invalid Argument at position ${i- -1}: \`${args[1]}\``);
+            }
         }
+       
+
 
         let lb = cs.getAll();
         if(lbsm != "all") lb = lb[lbsm];
@@ -296,29 +366,37 @@ bot.on("message", message => {
             
         }
         lb = lb.sort((a,b)=>(Object.values(b)[0] - Object.values(a)[0]));
+        let pages = [];
+        for (let i = 0, j = lb.length; i < j; i += 15) {
+            pages.push(lb.slice(i, i + 15));
+        }
+        if(pgct > pages.length || pgct <= 0) return reply("Invalid page index: `" + pgct + "`");
+        pgct--;
         let embed = new discord.MessageEmbed();
-        embed.setTitle((lbsm == "discord" ? "Discord" : lbsm == "twitch" ? "Twitch" : "Full") + " Leaderboard");
+        embed.setTitle(`${lbsm == "discord" ? "Discord" : lbsm == "twitch" ? "Twitch" : "Full"} Leaderboard (Page ${pgct+1}/${pages.length})`);
         embed.setColor("#1a7ce2");
         embed.setFooter("You can do !lb <discord|twitch> for a more specific leaderboard!");
         if(lbsm == "discord") {
             embed.setDescription(
-                lb.map(x=>`<@${Object.keys(x)[0]}>: ${Object.values(x)[0]} ${cs.type}`)
+                pages[pgct].map(x=>`<@${Object.keys(x)[0]}>: ${Object.values(x)[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}${cs.getRupee(Object.values(x)[0])}`)
             );
         }
         else if(lbsm == "twitch") {
             embed.setDescription(
-                lb.map(x=>`\`${Object.keys(x)[0]}\`: ${Object.values(x)[0]} ${cs.type}`)
+                pages[pgct].map(x=>`\`${Object.keys(x)[0]}\`: ${Object.values(x)[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}${cs.getRupee(Object.values(x)[0])}`)
             );
         }
         else {
             embed.setDescription(
-                lb.map(x=>`${Object.keys(x)[0]}: ${Object.values(x)[0]} ${cs.type}`)
+                pages[pgct].map(x=>`${Object.keys(x)[0]}: ${Object.values(x)[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}${cs.getRupee(Object.values(x)[0])}`)
             );
         }
         reply(embed);
         
         
     }
+    
+
     if(args[0] == "!test") {
        //nothing here atm
     }
