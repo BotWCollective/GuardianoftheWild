396d26e2aba93030322ef009934cef269a462f96
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-10-31 09:50:27 -0700

    recreate run_command to handle keywords

--- rs/src/gotw/command/command_map.rs
+++ rs/src/gotw/command/command_map.rs
@@ -1,90 +1,137 @@
 use super::{
-    commands::{Command, CommandPerms},
+    commands::{Command as Cmd, CommandPerms},
     CommandError,
 };
 use crate::{bot::Message, BotError, BotResult};
 use fasthash::{sea::Hash64, RandomState};
 use regex::{Regex, RegexBuilder};
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
+
+use BotError::Command;
+use CommandError::*;
 
 pub struct CommandMap {
-    trigger_re: Regex,
-    triggers: HashSet<String, RandomState<Hash64>>,
-    map: HashMap<String, Command, RandomState<Hash64>>,
+    keyword_re: Regex,
+    keywords: HashMap<String, Cmd, RandomState<Hash64>>,
+    commands: HashMap<String, Cmd, RandomState<Hash64>>,
 }
 
 impl CommandMap {
     pub fn new() -> Self {
         Self {
-            trigger_re: Regex::new("").unwrap(),
-            triggers: HashSet::with_hasher(RandomState::<Hash64>::new()),
-            map: HashMap::with_hasher(RandomState::<Hash64>::new()),
+            keyword_re: Regex::new("").unwrap(),
+            keywords: HashMap::with_hasher(RandomState::<Hash64>::new()),
+            commands: HashMap::with_hasher(RandomState::<Hash64>::new()),
         }
     }
-    pub fn run_command(&mut self, msg: Message) -> Option<String> {
-        if let Some(args) = msg.args {
-            let perms = CommandPerms::max(&msg.sender);
-            match msg.command.unwrap().as_str() {
-                "!commands" if args.len() == 2 && CommandPerms::Mod <= perms => {
-                    match args[0].as_str() {
-                        "remove" => {
-                            let res = self.map.remove(&args[1]);
-                            if res.is_some() {
-                                Some(format!("Command {} removed.", args[1]))
-                            } else {
-                                Some(format!("Command {} does not exist!", args[1]))
-                            }
+    pub fn lookup(&mut self, msg: Message) -> BotResult<Option<String>> {
+        if msg.words.is_empty() || msg.raw.is_empty() {
+            return Ok(None);
+        }
+        if self.commands.contains_key(&msg.words[0]) {
+            self.commands.get(&msg.words[0]).unwrap().run(msg.sender)
+        } else if msg.words[0] == "!commands" {
+            if CommandPerms::max(&msg.sender) >= CommandPerms::Mod && msg.words.len() > 2 {
+                match msg.words[1].as_str() {
+                    "add" => Ok(None),
+                    "alias" => {
+                        if msg.words.len() >= 4 {
+                            self.alias(
+                                &msg.words[2],
+                                &msg.words[3],
+                                if let Some(w) = msg.words.get(4) {
+                                    w == "-k"
+                                } else {
+                                    false
+                                },
+                            )
+                        } else {
+                            Err(Command(NotEnoughArgs))
                         }
-                        "show" => {
-                            if let Some(_) = self.map.get(&args[1]) {
-                                Some(format!("{}: command show as yet unimplemented", args[1]))
-                            } else {
-                                Some(format!("Command {} does not exist!", args[1]))
-                            }
-                        }
-                        _ => None,
                     }
+                    "del" => {
+                        if msg.words.len() >= 3 {
+                            self.delete(&msg.words[2])
+                        } else {
+                            Err(Command(NotEnoughArgs))
+                        }
+                    }
+                    "show" => Ok(None),
+                    _ => Ok(None),
                 }
-                "!alias" if args.len() == 2 && CommandPerms::Mod <= perms => {
-                    self.alias(&args[0], &args[1]);
-                    None
-                }
-                _ => None,
+            } else {
+                Ok(Some("this will be the list of commands eventually".into()))
             }
         } else {
-            None
+            let keywords: Vec<_> = self
+                .keyword_re
+                .find_iter(&msg.raw)
+                .map(|m| m.as_str().to_string())
+                .collect();
+            let keyword = keywords.iter().max().unwrap();
+            if self.keywords.contains_key(keyword) {
+                return self.keywords.get(keyword).unwrap().run(msg.sender);
+            }
+            Ok(None)
         }
     }
-    pub fn alias(&mut self, target: &str, alias: &str) {
-        if self.map.get(target).is_some() {
-            self.map
-                .insert(alias.to_string(), self.map.get(target).unwrap().clone());
+    fn alias(
+        &mut self,
+        target: &str,
+        alias: &str,
+        target_keyword: bool,
+    ) -> BotResult<Option<String>> {
+        if self.commands.get(target).is_some() {
+            if target_keyword {
+                self.keywords.insert(
+                    alias.to_string(),
+                    self.commands.get(target).unwrap().clone(),
+                );
+                self.refresh_keywords();
+            } else {
+                self.commands.insert(
+                    alias.to_string(),
+                    self.commands.get(target).unwrap().clone(),
+                );
+            }
+            Ok(Some(format!("{} aliased to {}", target, alias)))
+        } else if self.keywords.get(target).is_some() {
+            if target_keyword {
+                self.keywords.insert(
+                    alias.to_string(),
+                    self.commands.get(target).unwrap().clone(),
+                );
+                self.refresh_keywords();
+            } else {
+                self.commands.insert(
+                    alias.to_string(),
+                    self.commands.get(target).unwrap().clone(),
+                );
+            }
+            Ok(Some(format!("{} aliased to {}", target, alias)))
+        } else {
+            Err(Command(NotFound))
         }
     }
-    fn register_trigger(&mut self, trigger: &str) -> BotResult<()> {
-        if self.triggers.get(trigger).is_some() {
-            return Err(BotError::Command(CommandError::AlreadyRegistered));
+    fn delete(&mut self, target: &str) -> BotResult<Option<String>> {
+        if self.commands.remove(target).is_some() {
+            Ok(Some(format!("Command {} removed", target)))
+        } else if self.keywords.remove(target).is_some() {
+            self.refresh_keywords();
+            Ok(Some(format!("Keyword {} removed", target)))
+        } else {
+            Err(Command(NotFound))
         }
-        self.triggers.insert(trigger.to_owned());
-        self.refresh_triggers();
-        Ok(())
     }
-    fn unregister_trigger(&mut self, trigger: &str) -> BotResult<()> {
-        if !self.triggers.remove(trigger) {
-            return Err(BotError::Command(CommandError::NotFound));
-        }
-        self.refresh_triggers();
-        Ok(())
-    }
-    fn refresh_triggers(&mut self) {
-        let joined_triggers = self
-            .triggers
-            .iter()
+    fn refresh_keywords(&mut self) {
+        let joined_keywords = self
+            .keywords
+            .keys()
             .map(|t| regex::escape(t))
             .collect::<Vec<_>>()
             .join("|");
-        let re_str = format!(r"\b({})\b", joined_triggers);
+        let re_str = format!(r"\b({})\b", joined_keywords);
         let re_build = RegexBuilder::new(&re_str);
-        self.trigger_re = re_build.build().unwrap();
+        self.keyword_re = re_build.build().unwrap();
     }
 }
