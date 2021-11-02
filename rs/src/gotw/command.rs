ce30de5d16bc11b89d36ad3b8805c344900a2bf8
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-11-01 17:22:21 -0700

    remove CommandError::NotFound

--- rs/src/gotw/command.rs
+++ rs/src/gotw/command.rs
@@ -4,8 +4,6 @@ pub use command_map::CommandMap;
 
 #[derive(Debug)]
 pub enum CommandError {
-	// for regular command doesn't exist
-    NotFound,
     // for when a mod tries to alias/delete/show a command that doesn't exist
     NotRegistered,
     AlreadyRegistered,
